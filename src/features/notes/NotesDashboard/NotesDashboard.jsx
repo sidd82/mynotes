372e5109af267eb9024dfe904330920a31e76d76
siddcoder <psiddhesh14@gmail.com>
2018-06-30 16:08:51 +0530

    Edit Data Passing

--- src/features/notes/NotesDashboard/NotesDashboard.jsx
+++ src/features/notes/NotesDashboard/NotesDashboard.jsx
@@ -16,12 +16,21 @@ const notesDashboard = [
 class NotesDashboard extends Component {
   state = {
     notes: notesDashboard,
-    isOpen: false
+    isOpen: false,
+    selectedNote: null
   }
 
   handleFormOpen = () => {
     this.setState({
-      isOpen: !this.state.isOpen
+      isOpen: !this.state.isOpen,
+      selectedNote: null
+    })
+  }
+
+  handleSelectedNote = (selectedNote) => () => {
+    this.setState({
+      selectedNote: selectedNote,
+      isOpen: true
     })
   }
 
@@ -35,6 +44,7 @@ class NotesDashboard extends Component {
   }
 
   render() {
+    const {selectedNote} = this.state;
     return (
       <div>
         <div className="container">
@@ -43,9 +53,9 @@ class NotesDashboard extends Component {
             <button onClick={this.handleFormOpen} className="waves-effect waves-light btn orange darken-3">Create Notes</button>
             </div>
         </div>
-        {this.state.isOpen && <NotesForm createEvent={this.handleCreateNote} />}
+        {this.state.isOpen && <NotesForm selectedNote={selectedNote} createEvent={this.handleCreateNote} />}
         {this.state.notes.map((note) => (
-            <NotesList key={note.id} note={note}/>
+            <NotesList key={note.id} onNoteSelected={this.handleSelectedNote} note={note}/>
         ))}
         </div>
       </div>
--- src/features/notes/NotesList/NotesList.jsx
+++ src/features/notes/NotesList/NotesList.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 class NotesList extends Component {
   render() {
     const {note} = this.props;
+    const {onNoteSelected} = this.props
     return (
       <div className="row">
         <div className="col l12">
@@ -14,6 +15,7 @@ class NotesList extends Component {
             <div className="card-action">
               <a className="waves-effect waves-light btn push grey darken-1"><i className="material-icons left">access_time</i>{note.date}</a>
               <a className="waves-effect waves-light btn grey darken-1"><i className="material-icons left">account_circle</i>{note.author}</a>
+              <button onClick={onNoteSelected(note)} className="waves-effect waves-light btn orange darken-3 radius secondary-content"><i className="material-icons left">edit</i>Edit</button>
             </div>
           </div>
         </div>
